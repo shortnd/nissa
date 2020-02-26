38a52c7181aec27ce090dfff5046abfb9432938d
Collin OConnell <ococncol@gmail.com>
2020-02-26 09:59:09 -0500

    reworking setup

--- backend/models/team.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
-
-const TeamSchema = new Schema({
-  name: String,
-  players: Array
-});
-
-module.exports = mongoose.model.Team || mongoose.model('Team', TeamSchema)
